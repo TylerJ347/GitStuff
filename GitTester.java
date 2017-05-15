c6fb1b9f46714759fa1f5ab3b3948c1ae7d26020
TylerJ347 <ttjaafari@hotmail.com>
2017-05-15 11:02:09 -0400

    Add files via upload

---
+++ GitTester.java
@@ -0,0 +1,10 @@
+package gitStuffPackage;
+
+public class GitTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\"Don't let your memes be dreams.\"\n\t-Shia Labeouf");
+	}
+
+}
